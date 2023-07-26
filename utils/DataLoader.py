5507caaebb99224b6f98f39dfffc9cb84e939fc2
Igzi <igor.pavlovic.2001@gmail.com>
2023-07-26 21:00:14 +0200

    Dodata klasa DataLoaderLego

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -1,7 +1,3 @@
-import json
-import torch
-import imageio.v3 as iio
-
 class DataLoader():
     def __init__(self, config):
         self.dataset_path = config['dataset_path']
@@ -11,45 +7,3 @@ class DataLoader():
             self.validation_size = config['validation_size']
         if 'test_size' in config:
             self.test_size = config['test_size']
-
-    def getDataset(self, type):
-        if type == 'train':
-            dataset = 'train'
-            test_size = self.train_size
-        elif type == 'validation':
-            dataset = 'val'
-            test_size = self.validation_size
-        elif type == 'test':
-            dataset = 'test'
-            test_size = self.test_size
-        else:
-            print("Dataset type ", type, " does not exist")
-            return
-
-        transform_path = self.dataset_path + "/transforms_" + type + ".json"
-        transform_file = open(transform_path)
-        transforms = json.load(transform_file)
-
-        for i in range(test_size):
-            frame = transforms['frames'][0]
-            image_path = self.dataset_path + frame['file_path'] + ".png"
-            image = iio.imread(image_path)
-
-            if i==0:
-                images = torch.zeros((test_size,) + image.shape)
-                poses = torch.zeros((test_size,) + torch.tensor(frame['transform_matrix']).shape)
-
-            poses[i] = torch.tensor(frame['transform_matrix'])
-            images[i] = torch.tensor(image)
-
-        if(image.shape[-1]==4):
-            images = images[:,:,:,:3]
-
-        return images, poses
-
-
-
-
-        
-
-        
