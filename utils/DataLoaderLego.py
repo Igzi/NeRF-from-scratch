96462d0acfa1cd78bd18da7adb853b90246099fb
Drakula44 <ninadrakulic04@gmail.com>
2023-07-27 18:17:37 +0000

    Update DataLoaderLego.py

--- utils/DataLoaderLego.py
+++ utils/DataLoaderLego.py
@@ -45,7 +45,7 @@ class DataLoaderLego(DataLoader):
         H, W = images[0].shape[:2]
         focal = .5 * W / np.tan(.5 * transforms['camera_angle_x'])
         images = torch.nn.functional.interpolate(images.permute(
-            0, 3, 1, 2), (100, 100), mode='bilinear', align_corners=False).permute(0, 2, 3, 1)        
+            0, 3, 1, 2), (100, 100)).permute(0, 2, 3, 1)        
         images /= 255
         H = H//8
         W = W//8
