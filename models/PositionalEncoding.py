5b4b3069d0e208a8aa30d33ffbe895221bea837e
Drakula44 <ninadrakulic04@gmail.com>
2023-07-28 13:34:35 +0000

    fixed positional encoding pt2

--- models/PositionalEncoding.py
+++ models/PositionalEncoding.py
@@ -1,36 +1,30 @@
 import torch
 
-def sin_base_gen(n):
-    return lambda y: torch.sin(2**n*torch.pi*y)
-
-def cos_base_gen(n):
-    return lambda y: torch.cos(2**n*torch.pi*y)
-
 class PositionalEncoding(torch.nn.Module):
     def __init__(self, L: int, passIdentity=False):
         super().__init__()
         self.L = L
-        print(L)
-        sin_base = [sin_base_gen(i) for i in range(L)]
-        cos_base = [cos_base_gen(i) for i in range(L)]
+        sin_base = [(lambda x, i=i: torch.sin(2**i*torch.pi*x)) for i in range(L)]
+        cos_base = [(lambda x, i=i: torch.cos(2**i*torch.pi*x)) for i in range(L)]
         self.func_base = sin_base + cos_base
-        
-        
         if passIdentity:
-            self.func_base.append(lambda y: y)
-
-        self.out_features = len(self.func_base)
+            self.func_base.append(lambda x: x)
+        self.out_features = len(self.func_base) * 3
     
     def forward(self, x):
+        return [f(x) for f in self.func_base]
         return torch.cat([f(x) for f in self.func_base], dim=-1)
 
 if __name__ == "__main__":
-    print("Testing PositionalEncoding")
     import numpy as np
     import matplotlib.pyplot as plt
-    L = 6
-    posEnc = PositionalEncoding(L, True)
-    x = np.linspace(0, 1, 1000)
-    x = torch.tensor(x[..., None])
-    y = posEnc(x)
-    print(y.shape)
+
+    x = np.linspace(-1, 1, 1000)
+    x = torch.tensor(x, dtype=torch.float32)
+    x = x[None, :]
+    print(x.shape)
+    pe = PositionalEncoding(6)
+    y = pe(x)
+    for i in range(len(y)):
+        plt.plot(x[0], y[i][0].detach().numpy())
+        plt.show()
