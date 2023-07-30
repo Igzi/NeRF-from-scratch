c5170f7f9b6d53ae497efe272f422c7b5ff3a3c6
Igzi <igor.pavlovic.2001@gmail.com>
2023-07-30 13:41:26 +0200

    Dadat psiml dataset

--- utils/DataLoader.py
+++ utils/DataLoader.py
@@ -3,7 +3,13 @@ class DataLoader():
         self.dataset_path = config['dataset_path']
         if 'train_size' in config:
             self.train_size = config['train_size']
+        else:
+            self.train_size = 0
         if 'validation_size' in config:
             self.validation_size = config['validation_size']
+        else:
+            self.validation_size = 0
         if 'test_size' in config:
             self.test_size = config['test_size']
+        else:
+            self.test_size = 0
--- utils/DataLoaderReal.py
+++ utils/DataLoaderReal.py
@@ -1,15 +1,45 @@
 import json
 import torch
-import numpy as np
 import imageio.v3 as iio
 from utils.DataLoader import DataLoader
-import torchvision
 
 class DataLoaderReal(DataLoader):
     def __init__(self, config):
         super().__init__(config)
 
     def getDataset(self, type, downsample = False):
+        transform_path = self.dataset_path + "/transforms.json"
+        transform_file = open(transform_path)
+        transforms = json.load(transform_file)
+
+        focal = transforms['fl_x'], transforms['fl_y']
+
+        torch.manual_seed(0)
+        perm = torch.randperm(self.train_size + self.validation_size + self.test_size)
+
+        if type == 'train':
+            test_start = 0 
+            test_end = self.train_size
+        elif type == 'validation':
+            test_start = self.train_size 
+            test_end = self.train_size + self.validation_size
+        elif type == 'test':
+            test_start = self.train_size + self.validation_size
+            test_end = self.train_size + self.validation_size + self.test_size
+        else:
+            print("Dataset type ", type, " does not exist")
+            return
         
+        test_size = test_end - test_start
+        images = torch.zeros((test_size,) + image.shape)
+        poses = torch.zeros((test_size,) + torch.tensor(frame['transform_matrix']).shape)
+
+        for i in perm[test_start:test_end]:
+            frame = transforms['frames'][i]
+            image_path = self.dataset_path + frame['file_path'] + ".png"
+            image = iio.imread(image_path)
+
+            poses[i] = torch.tensor(frame['transform_matrix'])
+            images[i] = torch.tensor(image)
 
         return images, poses, focal
